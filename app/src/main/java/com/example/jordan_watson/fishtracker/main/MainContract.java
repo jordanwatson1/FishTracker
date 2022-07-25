0e044e914b8d19dad87cfbcb3a478d8ba4c2c739
Jordan Watson <jordanwatson915@gmail.com>
2022-07-25 12:21:50 -0700

    removed unnecessary space

--- app/src/main/java/com/example/jordan_watson/fishtracker/main/MainContract.java
+++ app/src/main/java/com/example/jordan_watson/fishtracker/main/MainContract.java
@@ -7,9 +7,13 @@ package com.example.jordan_watson.fishtracker.main;
 // defining contracts for view & presenter
 public interface MainContract {
     interface MvpView {
+        void showSignInScreen();
 
+        void showSignUpScreen();
     }
     interface Presenter{
+        void handleSignInButtonClick();
 
+        void handleSignUpButtonClick();
     }
 }
