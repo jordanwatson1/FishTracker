7f1215bfc6cd46465b38e910889ad2ef26ab403a
Jordan Watson <jordanwatson915@gmail.com>
2022-07-25 14:03:49 -0700

    implemented data binding

--- app/src/main/java/com/example/jordan_watson/fishtracker/main/MainContract.java
+++ app/src/main/java/com/example/jordan_watson/fishtracker/main/MainContract.java
@@ -1,5 +1,7 @@
 package com.example.jordan_watson.fishtracker.main;
 
+import android.view.View;
+
 /**
  * Defines the contract between the View {@link MainActivity} and the Presenter {@link MainPresenter}.
  */
@@ -12,8 +14,8 @@ public interface MainContract {
         void showSignUpScreen();
     }
     interface Presenter{
-        void handleSignInButtonClick();
+        void handleSignInButtonClick(View view);
 
-        void handleSignUpButtonClick();
+        void handleSignUpButtonClick(View view);
     }
 }
